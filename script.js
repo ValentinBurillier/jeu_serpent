2179de5a828b46f2f083249b3d7b9c48ef94de82
Valentin Burillier <valentin.burillier@gmail.com>
2024-08-02 18:32:07 +0200

    create snake & advance function

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ window.onload = function() {
   let blockSize = 30;
   let ctx;
   let delay = 1000;
-  let x = 0;
-  let y = 0;
+  let snakee;
   
   init();
 
@@ -17,21 +16,39 @@ window.onload = function() {
     canvas.style.border = '1px solid'; // bordure du canvas
     document.body.appendChild(canvas);
     ctx = canvas.getContext('2d'); //contexte du canvas en 2D
+    snakee = new Snake([[6,4], [5,4], [4,4]]); // Taille /représentation du serpent au début
     refreshCanvas();
   }
 
   function refreshCanvas() {
-    x += 20;
-    y += 20;
-    ctx.fillStyle = "#ff0000";
     ctx.clearRect(0,0,canvasWidth, canvasHeight);
-    ctx.fillRect(x, y, 100, 50);
+    snakee.draw();
+    snakee.advance();
     setTimeout(refreshCanvas, delay);
+  }
+
+  function drawblock(ctx, position) {
+    let x = position[0] * blockSize;
+    let y = position[1] * blockSize;
+    ctx.fillRect(x,y, blockSize, blockSize);
   }
 
   function Snake(body) {
     this.body = body;
-    this.draw = 
+    this.draw = function() {
+      ctx.save();
+      ctx.fillStyle = "#ff0000";
+      for(let i = 0; i < this.body.length; i++) {
+        drawblock(ctx, this.body[i]);
+      };
+      ctx.restore();
+    };
+    this.advance = function() { // fonction pour avancer le serpent toutes les n millisecondes
+      let nextPosition = this.body[0].slice();
+      nextPosition[0] += 1; // [7,4]
+      this.body.unshift(nextPosition); // now [[7,4], [6,4], [5,4], [4,4]]
+      this.body.pop(); // now [[7,4], [6,4], [5,4]]
+    }
   }
 
 }
